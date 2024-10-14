88d55f0f2e4a61564c9315107d9f6cb4ff966b83
testusuke <testusuke@gmail.com>
2024-10-14 23:32:49 +0900

    change: デモ用にアカウント無しで利用可能にした

--- app/api/auth/callback/route.ts
+++ app/api/auth/callback/route.ts
@@ -1,17 +1,9 @@
 import {NextRequest} from "next/server"
 
 export async function POST(request: NextRequest) {
-    //  form data
-    const form = await request.formData()
-    //  get state and code
-    const code = form.get('code')
-
     //  pass code to the backend
     const backendURL = process.env.NEXT_PUBLIC_API_INTERNAL_URL ? process.env.NEXT_PUBLIC_API_INTERNAL_URL : process.env.NEXT_PUBLIC_API_URL
     const loginEndpoint = backendURL + '/login'
-
-    //  redirect uri
-    const redirectUri = process.env.NEXT_PUBLIC_OIDC_REDIRECT_URL
 
     //  post code to the backend using fetch
     const response = await fetch(loginEndpoint, {
@@ -20,9 +12,7 @@ export async function POST(request: NextRequest) {
             'Content-Type': 'application/json',
         },
         body: JSON.stringify({
-            code: code,
-            redirect_uri: redirectUri
-        }),
+        })
     })
 
     //  get cookie from response
--- components/auth/LoginButton.tsx
+++ components/auth/LoginButton.tsx
@@ -1,47 +1,32 @@
 'use client'
 import {Button, Stack, Typography} from "@mui/material";
-import crypto from 'crypto';
-import * as querystring from "querystring";
-import {useEffect, useState} from "react";
 import MSLogo from "@/public/images/ms.svg";
 import * as React from "react";
 import {useTheme} from "@mui/material/styles";
+import {useRouter} from "next/navigation";
 
 export default function LoginButton() {
-    const theme = useTheme();
-    const [authorizationUrl, setAuthorizationUrl] = useState<string>('')
+    const theme = useTheme()
+    const subDirectory = process.env.SUB_DIRECTORY ? process.env.SUB_DIRECTORY : "/"
+    const router = useRouter()
 
-    useEffect(() => {
-        const authorizationBaseUrl = process.env.NEXT_PUBLIC_OIDC_AUTHORIZE_URL
-        //  query params
-        const clientId = process.env.NEXT_PUBLIC_OIDC_CLIENT_ID
-        const redirectUri = process.env.NEXT_PUBLIC_OIDC_REDIRECT_URL
-        const scope = process.env.NEXT_PUBLIC_OIDC_SCOPE ?? "openid profile email"
-        //  generate random nonce and state
-        const nonce = crypto.randomBytes(16).toString('hex')
-
-        const queryData = {
-            "client_id": clientId,
-            "redirect_uri": redirectUri,
-            "response_type": "code",
-            "response_mode": "form_post",
-            "scope": scope,
-            "nonce": nonce,
-        }
-
-        //  make query string
-        const searchParams = querystring.stringify(queryData);
-        //  make url
-        setAuthorizationUrl(`${authorizationBaseUrl}?${searchParams}`)
-    }, [])
-
-    const buttonDisplayName = process.env.NEXT_PUBLIC_OIDC_DISPLAY_NAME ?? "ログインできません"
+    const handleLogin = async () => {
+        await fetch("/api/auth/callback", {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+            })
+        })
+        router.replace(subDirectory)
+    }
 
     return (
         <Button
             variant="contained"
             color={"secondary"}
-            href={authorizationUrl}
+            onClick={handleLogin}
             sx={{px:3, py:2,mb:1, width:"100%", backgroundColor:`${theme.palette.text.primary}`}}
             disableElevation
         >
@@ -53,7 +38,7 @@ export default function LoginButton() {
             >
                 <MSLogo width={16} height={16}/>
                 <Typography fontSize={"14px"} color={theme.palette.background.paper}>
-                    {buttonDisplayName}
+                    ログイン
                 </Typography>
             </Stack>
         </Button>
